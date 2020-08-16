c1062ea1281cfaf8dc6c91a5e99558e7a8794d80
Ruvindu-droid <ruvinduagk@gmail.com>
2020-08-16 12:54:36 +0530

    It19062884 Initial Commit

---
+++ app/src/main/java/com/example/giftproject/EnterFinalDetailActivity2.java
@@ -0,0 +1,14 @@
+package com.example.giftproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EnterFinalDetailActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enter_final_detail2);
+    }
+}
